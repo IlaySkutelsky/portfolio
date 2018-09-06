4aa9d966de3b655df42f92cf99fe2447b05c3f73
Ilay Skutelsky <ilay.skutelsky@gmail.com>
2018-09-06 16:36:29 +0300

    Vcg: Update live demo

---
+++ precache-manifest.30a1119c8e76ff46fa4e9848fd92585a.js
@@ -0,0 +1,262 @@
+self.__precacheManifest = [
+  {
+    "revision": "b48746ad4ee5484c094f6f16021f748e",
+    "url": "/img/ufd_blue/ufo-sprite_12.png"
+  },
+  {
+    "revision": "6866804dc3b03a5669ea4c1b1ee115d3",
+    "url": "/img/ufd_blue/Untitled1.psd"
+  },
+  {
+    "revision": "71a7b5b4e92ffa1edab63b83a219bcf0",
+    "url": "/img/ufd_blue/_blue/Thumbs.db"
+  },
+  {
+    "revision": "ec688d6332b0b917d460",
+    "url": "/js/app.ec688d63.js"
+  },
+  {
+    "revision": "8ce5e2cd6e1254d0b97c",
+    "url": "/js/chunk-vendors.8ce5e2cd.js"
+  },
+  {
+    "revision": "4c5e21cfc67e818b4d3cdd925177113a",
+    "url": "/img/ufd_blue/_blue/blue_ufo_gmc_0011.png"
+  },
+  {
+    "revision": "034691f0b75c56f70089ebf40c1bc9d5",
+    "url": "/img/ufd_blue/_blue/blue_ufo_gmc_0010.png"
+  },
+  {
+    "revision": "23104717bfd01ed34c6893bd1a504104",
+    "url": "/img/ufd_blue/_blue/blue_ufo_gmc_0013.png"
+  },
+  {
+    "revision": "a94ba754a06a6c23885179db8fee2833",
+    "url": "/img/ufo.a94ba754.gif"
+  },
+  {
+    "revision": "c587540e21fcc4175c577192005571d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "0e54d8b64e1d2fd1dc69df0d611c756f",
+    "url": "/img/ufd_blue/ufo-sprite_0.png"
+  },
+  {
+    "revision": "d4ff88510264aef82f02fdc9f4e7f647",
+    "url": "/img/ufd_blue/ufo-sprite_1.png"
+  },
+  {
+    "revision": "4d9933d35e6a4dff21d8cfacf93d38ac",
+    "url": "/img/ufd_blue/ufo-sprite_15.png"
+  },
+  {
+    "revision": "b2ace1326af09b9b7a1286fe628b4127",
+    "url": "/img/ufd_blue/ufo-sprite_17.png"
+  },
+  {
+    "revision": "af3167040453656201fb9c1c40eecb6a",
+    "url": "/img/ufd_blue/ufo-sprite_16.png"
+  },
+  {
+    "revision": "d36c19b0e2b015268ab784b30bebdef5",
+    "url": "/img/ufd_blue/ufo-sprite_18.png"
+  },
+  {
+    "revision": "708dfd0b5eb99feaa96c8e02351f2148",
+    "url": "/img/ufd_blue/ufo-sprite_19.png"
+  },
+  {
+    "revision": "cf060282118f2d1fd9a68f8ecdfed152",
+    "url": "/img/ufd_blue/ufo-sprite_2.png"
+  },
+  {
+    "revision": "55b9213a1dbad56c96a16738d2efa4e0",
+    "url": "/img/ufd_blue/ufo-sprite_20.png"
+  },
+  {
+    "revision": "0934b250b3de71c08b1a8be9b607b57e",
+    "url": "/img/ufd_blue/ufo-sprite_23.png"
+  },
+  {
+    "revision": "7d9c4073c777dc9d2cb047dab109f32a",
+    "url": "/img/ufd_blue/ufo-sprite_22.png"
+  },
+  {
+    "revision": "6ac57ef5fcec6b59fecadb27bffa4a4f",
+    "url": "/img/ufd_blue/ufo-sprite_21.png"
+  },
+  {
+    "revision": "51f396644007813a91b6e700056cc561",
+    "url": "/img/ufd_blue/ufo-sprite_24.png"
+  },
+  {
+    "revision": "59fb2f5f30ef1cba41dafbce24858c9d",
+    "url": "/img/ufd_blue/ufo-sprite_3.png"
+  },
+  {
+    "revision": "c65463bea0cb32bec06dfbaa6fa3e137",
+    "url": "/img/ufd_blue/ufo-sprite_4.png"
+  },
+  {
+    "revision": "8b049d5d740ecb4a4485fccc55bdc836",
+    "url": "/img/ufd_blue/ufo-sprite_5.png"
+  },
+  {
+    "revision": "69759b7d41c6174951014a207687a948",
+    "url": "/img/ufd_blue/ufo-sprite_6.png"
+  },
+  {
+    "revision": "dcd425807cdaeec4ec5e6e12de7b3dfa",
+    "url": "/img/ufd_blue/ufo-sprite_10.png"
+  },
+  {
+    "revision": "e922fc8f3a60145dc8aa4369b2310c66",
+    "url": "/img/ufd_blue/ufo-sprite_11.png"
+  },
+  {
+    "revision": "98420e50c8a662c86fcda0d6543d178a",
+    "url": "/img/ufd_blue/ufo-sprite_13.png"
+  },
+  {
+    "revision": "36843c7ba6c7f4f410df3255d3dc0f33",
+    "url": "/img/ufd_blue/ufo-sprite_14.png"
+  },
+  {
+    "revision": "0078f85de0ffbe081bdf",
+    "url": "/js/about.0078f85d.js"
+  },
+  {
+    "revision": "0a015708cb52629bc12527e5766b27a5",
+    "url": "/img/ufd_blue/ufo-sprite_7.png"
+  },
+  {
+    "revision": "f02b875e794b24f2dd1befe84f9411ea",
+    "url": "/img/ufd_blue/ufo-sprite_8.png"
+  },
+  {
+    "revision": "34be75278eb361f1b03c2fa3d22d5882",
+    "url": "/img/ufd_blue/ufo-sprite_9.png"
+  },
+  {
+    "revision": "ab181835cd2a3a5e53c8b29d998999f8",
+    "url": "/img/ufd_blue/_blue/blue_ufo_gmc_0000.png"
+  },
+  {
+    "revision": "ed157a5b6a25fe4d1b20eaeb0877bdc1",
+    "url": "/img/ufd_blue/_blue/blue_ufo_gmc_0020.png"
+  },
+  {
+    "revision": "6e732adc6aed64a0a33ea2f3009dbfe6",
+    "url": "/img/ufd_blue/_blue/blue_ufo_gmc_0021.png"
+  },
+  {
+    "revision": "d690fc2e2195226f15fedca3e6bc9131",
+    "url": "/img/ufd_blue/_blue/blue_ufo_gmc_0023.png"
+  },
+  {
+    "revision": "c47699cd29e5a8b1a68904960c9fb5ee",
+    "url": "/img/ufd_blue/_blue/blue_ufo_gmc_0022.png"
+  },
+  {
+    "revision": "9c30b63809d4dc74b28c84282baaf4c5",
+    "url": "/img/ufd_blue/_blue/blue_ufo_gmc_0024.png"
+  },
+  {
+    "revision": "9b9503409610ed42fee347a05f8b469f",
+    "url": "/img/ufd_blue/_blue/blue_ufo_gmc_0001.png"
+  },
+  {
+    "revision": "767675aa8e7b14e2538ac3f8e543c2a6",
+    "url": "/img/ufd_blue/_blue/blue_ufo_gmc_0018.png"
+  },
+  {
+    "revision": "8df26cc835f31c133a99a4027c57766c",
+    "url": "/img/ufd_blue/_blue/blue_ufo_gmc_0019.png"
+  },
+  {
+    "revision": "7b11215fdc973653ca06b155ba54a673",
+    "url": "/img/ufd_blue/_blue/blue_ufo_gmc_0003.png"
+  },
+  {
+    "revision": "ce4ef158e0281c596376fb00c018e569",
+    "url": "/img/ufd_blue/_blue/blue_ufo_gmc_0002.png"
+  },
+  {
+    "revision": "77baa27e53263fa4b8abdd5a84d30ced",
+    "url": "/img/ufd_blue/_blue/blue_ufo_gmc_0017.png"
+  },
+  {
+    "revision": "1326a8127b1d382cb4a70b42f1077dff",
+    "url": "/img/ufd_blue/_blue/blue_ufo_gmc_0004.png"
+  },
+  {
+    "revision": "319900d81c1b55cf0d92e3b4b3d0135a",
+    "url": "/img/ufd_blue/_blue/blue_ufo_gmc_0015.png"
+  },
+  {
+    "revision": "8581f6de211956a593d0adb5bf754853",
+    "url": "/img/ufd_blue/_blue/blue_ufo_gmc_0016.png"
+  },
+  {
+    "revision": "89f7a41861cbb35d7264c4c08ba300fd",
+    "url": "/img/ufd_blue/_blue/blue_ufo_gmc_0005.png"
+  },
+  {
+    "revision": "648a69d88e5a9431d426640f0782d024",
+    "url": "/img/ufd_blue/_blue/blue_ufo_gmc_0006.png"
+  },
+  {
+    "revision": "fa41164487082921a46aced5e64a408c",
+    "url": "/img/ufd_blue/_blue/blue_ufo_gmc_0014.png"
+  },
+  {
+    "revision": "0b26de412f44da25d2a76250c89facbd",
+    "url": "/img/ufd_blue/_blue/blue_ufo_gmc_0007.png"
+  },
+  {
+    "revision": "2ee32eceb093c9d9a3cbb2dc69bbb008",
+    "url": "/img/ufd_blue/_blue/blue_ufo_gmc_0008.png"
+  },
+  {
+    "revision": "e3fcc20b174b03101d6baaff586c1740",
+    "url": "/img/ufd_blue/_blue/blue_ufo_gmc_0009.png"
+  },
+  {
+    "revision": "444fef4a6731a3e782ed25cd81b2b250",
+    "url": "/img/ufd_blue/_blue/blue_ufo_gmc_0012.png"
+  },
+  {
+    "revision": "dc6625eae7dd519c6007d94333fb70f4",
+    "url": "/img/ufd_blue/Thumbs.db"
+  },
+  {
+    "revision": "1fa122b00023497a98ec6d1f87c77571",
+    "url": "/img/projects-previews/TravelMaker.png"
+  },
+  {
+    "revision": "c1edee4681ab5de99433834cfcfc92b9",
+    "url": "/img/autumn-landscape-1352568333n2y.c1edee46.png"
+  },
+  {
+    "revision": "58b5e41ac9d17a574eb0aaf16ea4b7f4",
+    "url": "/fonts/Corben-Regular.58b5e41a.ttf"
+  },
+  {
+    "revision": "8c6847c75ae35d0ca5fd3798d4567443",
+    "url": "/fonts/AbrilFatface-Regular.8c6847c7.ttf"
+  },
+  {
+    "revision": "ec688d6332b0b917d460",
+    "url": "/css/app.c585419c.css"
+  },
+  {
+    "revision": "0078f85de0ffbe081bdf",
+    "url": "/css/about.9d96163d.css"
+  }
+];
